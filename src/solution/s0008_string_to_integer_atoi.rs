33a6cf1a3b675a1ccd167348849eaaf5df555815
DawnDantes <dawndantes@163.com>
2023-06-08 10:53:23 +0800

    feature: solve 8_string_to_integer_atoi

---
+++ src/solution/s0008_string_to_integer_atoi.rs
@@ -0,0 +1,126 @@
+/**
+ * [8] String to Integer (atoi)
+ *
+ * Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
+ * The algorithm for myAtoi(string s) is as follows:
+ * <ol>
+ * 	Read in and ignore any leading whitespace.
+ * 	Check if the next character (if not already at the end of the string) is '-' or '+'. Read this character in if it is either. This determines if the final result is negative or positive respectively. Assume the result is positive if neither is present.
+ * 	Read in next the characters until the next non-digit character or the end of the input is reached. The rest of the string is ignored.
+ * 	Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32). If no digits were read, then the integer is 0. Change the sign as necessary (from step 2).
+ * 	If the integer is out of the 32-bit signed integer range [-2^31, 2^31 - 1], then clamp the integer so that it remains in the range. Specifically, integers less than -2^31 should be clamped to -2^31, and integers greater than 2^31 - 1 should be clamped to 2^31 - 1.
+ * 	Return the integer as the final result.
+ * </ol>
+ * Note:
+ *
+ * 	Only the space character ' ' is considered a whitespace character.
+ * 	Do not ignore any characters other than the leading whitespace or the rest of the string after the digits.
+ *
+ *  
+ * <strong class="example">Example 1:
+ *
+ * Input: s = "42"
+ * Output: 42
+ * Explanation: The underlined characters are what is read in, the caret is the current reader position.
+ * Step 1: "42" (no characters read because there is no leading whitespace)
+ *          ^
+ * Step 2: "42" (no characters read because there is neither a '-' nor '+')
+ *          ^
+ * Step 3: "<u>42</u>" ("42" is read in)
+ *            ^
+ * The parsed integer is 42.
+ * Since 42 is in the range [-2^31, 2^31 - 1], the final result is 42.
+ *
+ * <strong class="example">Example 2:
+ *
+ * Input: s = "   -42"
+ * Output: -42
+ * Explanation:
+ * Step 1: "<u>   </u>-42" (leading whitespace is read and ignored)
+ *             ^
+ * Step 2: "   <u>-</u>42" ('-' is read, so the result should be negative)
+ *              ^
+ * Step 3: "   -<u>42</u>" ("42" is read in)
+ *                ^
+ * The parsed integer is -42.
+ * Since -42 is in the range [-2^31, 2^31 - 1], the final result is -42.
+ *
+ * <strong class="example">Example 3:
+ *
+ * Input: s = "4193 with words"
+ * Output: 4193
+ * Explanation:
+ * Step 1: "4193 with words" (no characters read because there is no leading whitespace)
+ *          ^
+ * Step 2: "4193 with words" (no characters read because there is neither a '-' nor '+')
+ *          ^
+ * Step 3: "<u>4193</u> with words" ("4193" is read in; reading stops because the next character is a non-digit)
+ *              ^
+ * The parsed integer is 4193.
+ * Since 4193 is in the range [-2^31, 2^31 - 1], the final result is 4193.
+ *
+ *  
+ * Constraints:
+ *
+ * 	0 <= s.length <= 200
+ * 	s consists of English letters (lower-case and upper-case), digits (0-9), ' ', '+', '-', and '.'.
+ *
+ */
+pub struct Solution {}
+
+// problem: https://leetcode.com/problems/string-to-integer-atoi/
+// discuss: https://leetcode.com/problems/string-to-integer-atoi/discuss/?currentPage=1&orderBy=most_votes&query=
+
+// submission codes start here
+
+impl Solution {
+    pub fn my_atoi(s: String) -> i32 {
+        let mut ans = 0;
+        let mut sign = 1;
+        let len = s.len();
+        let mut i: usize = 0;
+        let chars: Vec<char> = s.chars().collect();
+        while i < len && chars[i] == ' ' {
+            i = i + 1;
+        }
+        if i == len {
+            return ans;
+        }
+        match chars[i] {
+            '-' => {
+                sign = -1;
+                i = i + 1;
+            }
+            '+' => {
+                i = i + 1;
+            }
+            _ => {}
+        }
+        while i < len {
+            if chars[i] >= '0' && chars[i] <= '9' {
+                if ans > i32::MAX / 10 || ans == i32::MAX / 10 && chars[i] > '7' {
+                    return if sign == 1 { i32::MAX } else { i32::MIN };
+                }
+                ans = ans * 10 + (chars[i] as i32 - 48);
+            } else {
+                break;
+            }
+            i = i + 1;
+        }
+        return ans * sign;
+    }
+}
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_8() {
+        assert_eq!(Solution::my_atoi(String::from("42")), 42);
+        assert_eq!(Solution::my_atoi(String::from("   -42")), -42);
+        assert_eq!(Solution::my_atoi(String::from("4193 with words")), 4193);
+    }
+}
